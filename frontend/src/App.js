d77edf61ac8b40972420ad8cbaba271537daeec1
soumyasootar <soumyaswaroopsootar@gmail.com>
2023-04-23 22:41:31 +0530

    FrontEnd Created

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import ChatPage from "./Components/ChatPage";
+import HomePage from "./Components/HomePage";
+
+
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/chats" element={<ChatPage/>}></Route>
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
