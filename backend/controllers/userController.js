af41f6ed80a5aafab7d346651dc45785f1fe41a1
soumyasootar <soumyaswaroopsootar@gmail.com>
2023-04-29 17:13:51 +0530

    Register and Login Apis Created

---
+++ backend/controllers/userController.js
@@ -0,0 +1,80 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../Models/UserModel");
+const generateToken = require("../configs/generateToken");
+
+module.exports.registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  if (!name) {
+    res.status(400);
+    throw new Error("Please Enter Name");
+  }
+  if (!email) {
+    res.status(400);
+    throw new Error("Please Enter Email");
+  }
+  if (!password) {
+    res.status(400);
+    throw new Error("Please Enter Password");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("This User Exists Try Logging in");
+  }
+
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (newUser) {
+    res.status(201).json({
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      pic: newUser.pic,
+      token: generateToken(newUser._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed To Create User");
+  }
+});
+
+module.exports.authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    res.status(400);
+    throw new Error("Please Enter Email");
+  }
+  if (!password) {
+    res.status(400);
+    throw new Error("Please Enter Password");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (!userExists) {
+    res.status(400);
+    throw new Error("This User Doesnt Exist Try Registering User");
+  } else if (userExists && (await userExists.matchPass(password))) {
+    res.status(201).json({
+      _id: userExists._id,
+      name: userExists.name,
+      email: userExists.email,
+      pic: userExists.pic,
+      token: generateToken(userExists._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("This user exists, but password does not Match");
+  }
+});
+
+// module.exports = {registerUser , authUser};
